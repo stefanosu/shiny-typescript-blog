76c696b3f67321143849318e88d1f6fabf3165f4
Stefanos <stefanosu422@gmail.com>
2021-01-09 00:36:40 -0500

    added user controller

---
+++ backend/server/controllers/user.js
@@ -0,0 +1,21 @@
+import model from '../models';
+
+
+class Users {
+  static signUp(req, res) {
+    const { username, email, password } = req.body
+      return User
+        .create({
+          username,
+          email,
+          password
+        })
+        .then(userData => res.status(201).send({
+          success: true,
+          message: 'User successfully created',
+          userData
+        }))
+    }
+}
+
+export default Users;
