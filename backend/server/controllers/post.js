22317780248dae4f253948484a32cb5da931232c
Stefanos <stefanosu422@gmail.com>
2021-01-09 16:04:01 -0500

    added delete and update posts action in controller

--- backend/server/controllers/post.js
+++ backend/server/controllers/post.js
@@ -4,9 +4,9 @@ const { Post } = model;
 
 class Posts {
   static list(req, res) {
-    return Book
+    return Post
       .findAll()
-      .then(books => res.status(200).send(books));
+      .then(posts => res.status(200).send(posts));
   }
   static create(req, res) {
     const { title, content, favorite } = req.body
@@ -21,8 +21,49 @@ class Posts {
       .then(post => res.status(201).send({
         message: `Your blog post with the title ${title} has been created successfully `,
         post
-      }))
-      
+      })) 
+    }
+    static modify(req, res) {
+      const { title, content, favorite } = req.body
+      return Post
+        .findById(req.params.postId)
+        .then((post) => {
+          post.update({
+            title: title || post.title,
+            content: content || post.content,
+            favorite: favorite || post.favorite,
+          })
+          .then((updatedPost) => {
+            res.status(200).send({
+              message: 'Post updated successfully',
+              data: {
+                title: title || updatedPost.title,
+                content: content || updatedPost.content,
+                favorite: favorite || updatedPost.favorite,
+              }
+            })
+          })
+          .catch(error => res.status(400).send(error));
+        })
+        .catch(error => res.status(400).send(error));
+    }
+    static delete(req, res) {
+      return Post
+        .findById(req.params.Id)
+        .then(post => {
+          if(!post) {
+            return res.status(400).send({
+            message: 'Post Not Found',
+            });
+          }
+          return post
+            .destroy()
+            .then(() => res.status(200).send({
+              message: 'Post successfully deleted'
+            }))
+            .catch(error => res.status(400).send(error));
+        })
+        .catch(error => res.status(400).send(error))
     }
 }
 
--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -8,8 +8,13 @@ app.get('/api', (req, res) => res.status(200).send({
 }));
 
 app.post('/api/users', Users.signUp); // API route for user to signup
+
 app.post('/api/users/:userId/posts', Posts.create); // API route for user to create a post
 
 
 app.get('/api/posts', Posts.list); // API route for user to get all posts in the database
-};
+
+
+app.put('/api/posts/:postID', Posts.modify); // API route for user to edit a post
+
+}
