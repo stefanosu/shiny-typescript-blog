5ab59d681d3c66dbb40a2136f302345b2d1da27d
Stefanos <stefanosu422@gmail.com>
2021-01-13 03:02:06 -0500

    added some styling to create post form

--- frontend-blog/src/components/NewPost.tsx
+++ frontend-blog/src/components/NewPost.tsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { BrowserRouter as Router, Route, Link, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 export interface NewPostProps {
   handleChange: (
@@ -10,7 +16,10 @@ export interface NewPostProps {
 }
 
 const NewPost = ({ savePost, handleChange }: NewPostProps) => {
-  const { register, handleSubmit, setValue, errors } = useForm<NewPostProps>({});
+  const [submitted, setSubmitted] = useState(false);
+  const { register, handleSubmit, setValue, errors } = useForm<NewPostProps>(
+    {}
+  );
 
   const [newPost, setNewPost] = useState<NewPostProps>();
 
@@ -19,51 +28,57 @@ const NewPost = ({ savePost, handleChange }: NewPostProps) => {
   });
 
   const createNewPost = async (postData: any) => {
-    console.log(postData)
-    const userId = localStorage.getItem('user') || '';
+    console.log(postData);
+    const userId = localStorage.getItem("user") || "";
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({...postData, userId})
+      body: JSON.stringify({ ...postData, userId }),
     };
     const response = await fetch(
       "http://localhost:3000/api/createPosts",
       requestOptions
     );
     const data = await response.json();
-    setNewPost(data); 
-    //after request I would like to redirect to another route to go to see all posts 
-    <Redirect to="/allPosts" />
-
+    setNewPost(data);
+    //after request I would like to redirect to another route to go to see all posts
+    // if(submitted) {
+    //   return <Redirect push to={{
+    //     pathname: '/allPosts'
+    //   }}
+    // }
   };
-  
-
 
   return (
-  <form onSubmit={onSubmit}>
-    <h1>Create New Post</h1>
-    <input
-      ref={register({ required: true })}
-      type="text"
-      onChange={handleChange}
-      placeholder="title"
-      size={39}
-      required
-      name="title"
-    ></input>
+    <div className="container">
+      <div className='inner'>  
+      <form className="post-form" onSubmit={onSubmit}>
+        <h1 className='title'>Create New Post</h1>
+        <input className='input'
+          ref={register({ required: true })}
+          type="text"
+          onChange={handleChange}
+          placeholder="title"
+          size={39}
+          required
+          name="title"
+        ></input>
 
-    <textarea
-      ref={register({ required: true })}
-      onChange={handleChange}
-      placeholder="contents"
-      rows={8}
-      cols={41}
-      required
-      name="content"
-    ></textarea>
+        <textarea className='content-form'
+          ref={register({ required: true })}
+          onChange={handleChange}
+          placeholder="contents"
+          rows={8}
+          cols={41}
+          required
+          name="content"
+        ></textarea>
 
-    <button type="submit">Create Post</button>
-  </form>);
+        <button className='btn-post' type="submit">Create Post</button>
+      </form>
+    </div>
+    </div>
+  );
 };
 
 NewPost.defaultProps = {
